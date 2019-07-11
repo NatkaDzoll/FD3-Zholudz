cf9da80c4d42fcf33c3864011a9e57a54feadfa0
NatkaDzoll <natalia.zholud@mail.ru>
2019-07-11 14:27:29 +0300

    create RainbowFrame function

--- RainbowFrame/App.js
+++ RainbowFrame/App.js
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import RainbowFrame from './components/RainbowFrame';
 
 let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
