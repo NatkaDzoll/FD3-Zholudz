e5eaaaa993153220391eca2de4307be30975ad35
NatkaDzoll <natalia.zholud@mail.ru>
2019-07-16 09:52:47 +0300

    pass any properties to the component

--- RainbowFrameHOC/components/withRainbowFrame.js
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -5,7 +5,7 @@ import './RainbowFrame.css';
 let withRainbowFrame = colors =>  (Component) => props => {
   let colorFrame = colors.reduce((startEl, color) => {
     return <div style={{border: "solid 10px " + color, padding: "10px"}}>{startEl}</div>
-  }, <Component>{props.children}</Component>); // передаем стартовый компонент это текст Hello!
+  }, <Component {...props} />); // передаем стартовый компонент это текст Hello!
 
       return <div className="RainbowFrame">{colorFrame}</div>
 };
