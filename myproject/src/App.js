4e995e9a7cf801141694b4852617c8190dad7e08
NatkaDzoll <natalia.zholud@mail.ru>
2019-08-16 20:11:23 +0300

    create myproject

---
+++ myproject/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter } from 'react-router-dom';
+import PagesLinks from './pages/PagesLinks';
+import PagesRouter from './pages/PageRouter';
+
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import combinedReducer from './redux/reducers';
+
+let store = createStore(combinedReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+function App() {
+  return (
+    <Provider store={store}>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <PagesLinks/>
+          {/* <img src={logo} className="App-logo" alt="logo" />*/}
+        </header>
+
+        <main className="App-main">
+          <PagesRouter/>
+        </main>
+
+        <footer className="App-footer">
+        </footer>
+      </div>
+    </BrowserRouter>
+    </Provider>
+  );
+}
+export default App;
