bdb37956ff42ecd08ee671cdb4c8a8c29795adf3
NataliaZholud <nz@imlab.by>
2020-01-15 15:32:56 +0300

    my project build

---
+++ myproject/build/precache-manifest.fff9abdfa4c5cbfd4af2822152bf0f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd91f59777d522ee05897d496ce594a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "650f35c39ff496fbeca1",
+    "url": "/static/css/main.872fbdee.chunk.css"
+  },
+  {
+    "revision": "0c1ebdb715386e44af05",
+    "url": "/static/js/2.0a896ea5.chunk.js"
+  },
+  {
+    "revision": "650f35c39ff496fbeca1",
+    "url": "/static/js/main.a9ea17c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3993f2877b1099bf536a37798bcfd4ba",
+    "url": "/static/media/pencil_color_blue.3993f287.ico"
+  }
+]);
